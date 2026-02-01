2b4c8ba248ea7867c82f0a8179c3e225f2a08b04
thuycin <thuymamers@gmail.com>
2026-02-01 12:23:06 +0200

    Add 01.Thuy.homework 1

---
+++ 01.Thuy.homework1.py
@@ -0,0 +1,23 @@
+# NUMBER 1 - Age calculator
+year_of_birth = input("Please enter your YoB (yyyy): ")
+current_year = 2026
+age = current_year - int(year_of_birth)
+print(age)
+
+# NUMBER 2 - Code Missing Part
+x = 152
+y = 132
+print(x / y)
+print(13*(x / y))
+print(13*(x / y)**0.5)
+print(int(float(13*(x / y)**0.5)))
+
+# NUMBER 3 - Combine code into a separable variable
+code_1 = '354'
+code_3 = 132
+new_code = code_1 + '-' + str(code_3)
+print(new_code)
+
+# NUMBER 4 - Output a string using available variables
+name = input("Please enter your name: ")
+print("Hello ", name, "." "You are ", age, "years old. Your secret code is ", new_code, ".")
