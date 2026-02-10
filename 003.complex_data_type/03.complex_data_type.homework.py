ca6842f08e5d4bad99bcbbb2a9233525ebb0cf7f
thuycin <thuymamers@gmail.com>
2026-02-10 18:16:45 +0200

    corrected

--- 003.complex_data_type/03.complex_data_type.homework.py
+++ 003.complex_data_type/03.complex_data_type.homework.py
@@ -1,13 +1,14 @@
 # Difference
 set_a = {1, 2, 3, 4}
 set_b = {3, 4, 5, 6}
-print(set_a.difference(set_b))
-print(set_b.difference(set_a))
+print('Set A unique items:', set_a.difference(set_b))
+print('Set B unique items:',set_b.difference(set_a))
 
 # Create a string from a list and save it to variable
 # Each name one new line
 names = ['Jack', 'Mary', 'Samantha', 'George', 'Simon', 'John']
 print(*names, sep="\n")
+print('\n'.join(names))
 
 # sum of all numbers in a list
 numbers = [2, 53, 12, 87, 65, 32, 12, 2, 65, 32]
@@ -20,10 +21,8 @@ print(sum(set(numbers)))
 # make sure there is no duplicates in a new lists
 studentsA = ['Jack', 'Bob', 'Mary']
 studentsB = ['Bob', 'Sarah', 'Simon']
-A = set(studentsA)
-B = set(studentsB)
-print(A.union(B))
-
+studentsC = list(set([*studentsA, *studentsB]))
+print(studentsC)
 # What elements are common for both tuples?
 numbersA = (23, 52, 12, 75, 42)
 numbersB = (75, 22, 42, 94, 70)
@@ -39,3 +38,9 @@ lst.append(5)
 lst.sort
 a = tuple(lst)
 print(a)
+# b = a[:4] + (5,) + a[4:]
+# print(b)
+# a = list(a)
+# a.insert(4,5)
+# a = tuple(a)
+# print(a)
